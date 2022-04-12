3273dabdeebccc2057e06dab7c23d4e6c837a6ae
“Kahiroshi” <bluemoonapple404@gmail.com>
2022-04-12 14:39:45 +0800

    upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Web_Project_V1.0
+
+明新科技大學<br/>
+110-2 商用程式設計期末專題<br/>
+專題名稱：網頁解謎遊戲<br/>
+工具：Microsoft Visual Studio ASP.NET MVC<br/>
+
+HTTPS : https://github.com/Zwillinge268/Web_Project_V1.0.git
---
+++ Web_Project/Web_Project/Controllers/PuzzleController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Project.Controllers
+{
+    public class PuzzleController : Controller
+    {
+        // GET: Puzzle
+        public ActionResult Level1()
+        {
+            ViewBag.Message = " - Hit.請輸入答案";
+            return View();
+        }
+        public ActionResult Level2()
+        {
+            ViewBag.Message = " - Hit. print(\"1\" + \"1\")";
+            return View();
+        }
+        public ActionResult Level3()
+        {
+            return View();
+        }
+        public ActionResult Level4()
+        {
+            return View();
+        }
+        public ActionResult Level5()
+        {
+            return View();
+        }
+        public ActionResult Level6()
+        {
+            return View();
+        }
+        public ActionResult Level7()
+        {
+            return View();
+        }
+        public ActionResult Level8()
+        {
+            return View();
+        }
+    }
+}
